d3564262314520dcddd07067f0253b2c4a685c67
Luciano Mammino <luciano.mammino@fourtheorem.com>
2022-05-06 17:48:29 +0100

    Added comments in solution

--- 01-intro/exercises/champions.solution.js
+++ 01-intro/exercises/champions.solution.js
@@ -4,7 +4,9 @@
  */
 export default function champions (athletes) {
   const wonAtLeast3Medals = []
+  // Using for...of in combination with Object.entries to iterate over all the key-value pairs
   for (const [athlete, medals] of Object.entries(athletes)) {
+    // Using reduce as a shortcut. Could have used another for...of and Object.values
     const countMedals = Object.values(medals).reduce((acc, curr) => acc + curr)
     if (countMedals >= 3) {
       wonAtLeast3Medals.push(athlete)
